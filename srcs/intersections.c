7e2c572e0a8c493bf2be3561d2d8dc652af19203
Paulo Gaspar <pgaspar@c1r6s6.42luanda.com>
2025-03-24 12:52:03 +0100

    com luz adicionada quase perfeitamente

--- srcs/intersections.c
+++ srcs/intersections.c
@@ -32,6 +32,23 @@ int	intersect_sphere(t_sphere *sphere, t_point direction, t_minirt *rt, double *
 	return (1);
 }
 
+int	intersect_plane(t_minirt *rt, t_plane *plane, t_point direction, double *t)
+{
+	double	npmo;
+	double	nd;
+	t_point	pmo;
+	
+	pmo = vecdif(plane->coordinates, rt->camera.coordinates);
+	npmo = escprod(plane->n_vector, pmo);
+	nd = escprod(plane->n_vector, direction);
+	if (nd == 0)
+		return (0);
+	t[0] = npmo / nd;
+	if (t[0] < 0)
+		return (0);
+	return (1);
+}
+
 t_color	intersect_scene(t_point direction, t_minirt *rt)
 {
 	double		t[2];
@@ -58,6 +75,24 @@ t_color	intersect_scene(t_point direction, t_minirt *rt)
 				bateu = vecsoma(rt->camera.coordinates, vecprodesc(direction, rt->closest));
 				normal = vecdif(bateu, rt->sphere[i].coordinates);
 				normal = vecnorm(normal);
+				color = add_alight(color, rt);
+				color = add_dlight(rt, color, old_color, bateu, normal);
+			}
+		}
+		i++;
+	}
+	i = 0;
+	while (i < rt->pl)
+	{
+		if (intersect_plane(rt, &rt->plane[i], direction, t))
+		{
+			if (t[0] > 0 && t[0] < rt->closest)
+			{
+				rt->closest = t[0];
+				color = rt->plane[i].color;
+				old_color = color;
+				bateu = vecsoma(rt->camera.coordinates, vecprodesc(direction, rt->closest));
+				normal = rt->plane[i].n_vector;
 				color = add_alight(color, rt);
 				color = add_dlight(rt, color, old_color, bateu, normal);
 			}
--- srcs/lightapp.c
+++ srcs/lightapp.c
@@ -22,26 +22,6 @@ t_color add_alight(t_color color, t_minirt *rt)
     return (new);
 }
 
-/* t_color add_dlight(t_minirt *rt, t_color color, t_point bateu, t_point normal)
-{
-    t_point lightdir;
-    t_color dif_color;
-    t_color last_color;
-    double  esc;
-
-    lightdir = vecnorm(vecdif(rt->light.coordinates, bateu));
-    esc = escprod(normal, lightdir);
-    if (esc < 0)
-        return(color);
-    dif_color.r = (color.r * rt->light.color.r / 255.0) * rt->light.ratio * esc;
-    dif_color.g = (color.g * rt->light.color.g / 255.0) * rt->light.ratio * esc;
-    dif_color.b = (color.b * rt->light.color.b / 255.0) * rt->light.ratio * esc;
-    last_color.r = fmin(255, color.r + dif_color.r);
-    last_color.g = fmin(255, color.g + dif_color.g);
-    last_color.b = fmin(255, color.b + dif_color.b);
-    return (last_color);
-} */
-
 t_color add_dlight(t_minirt *rt, t_color color, t_color old_color, t_point bateu, t_point normal)
 {
     t_point lightdir;
